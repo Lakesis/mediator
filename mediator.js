b4b4b61806b11d378e322631672de51a0ca718b2
Pablo Gil <lakesis@gmail.com>
2012-07-13 16:37:44 +1000

    Fine tuning

--- mediator.js
+++ mediator.js
@@ -8,30 +8,26 @@
 var mediator = (function(mediator, undefined){
 
 	var subscribers = [];
-	//	ATTACHING METHOD
+
 	mediator.on = function(message, fn){
-		// Custom events
 		if (typeof subscribers[message] == "undefined"){
 			subscribers[message] = [];
 		}
 		subscribers[message].push({'context':this,'action': fn});
-		
+
 		return this;
 	};
-	
-	// FIRING METHOD	
+		
 	mediator.publish = function(message){
 		if (!subscribers[message]) return false;
 		var thisSubscribers = subscribers[message],
 		args = Array.prototype.slice.call(arguments, 1);
-		
-		for (var i=0, length = thisSubscribers.length; i < length; i++){	// Handler iteration 
+		for (var i=0, length = thisSubscribers.length; i < length; i++){
 			thisSubscribers[i].action.call(thisSubscribers[i].context, args);
 		}
 		return this
 	};
 	
-	// REMOVING METHOD
 	mediator.off = function(message){
 		if (!subscribers[message]) return false;
 		var thisSubscribers = subscribers[message];
@@ -44,9 +40,10 @@ var mediator = (function(mediator, undefined){
 		object.on = mediator.on;
 		object.publish = mediator.publish;
 		object.off = mediator.off;
+		
+		return this;
 	};
 		
 	 return mediator;
-
 	
 })(mediator || {}, window, document);
